da7fc0f06ebcd9836f149d26165c1cf31ff7b9f6
vince-vibin <vincent.sternad@meconema.de>
2025-06-26 20:03:14 +0200

    add carousel

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
-function App() {
+import { MantineProvider, createTheme, type MantineColorsTuple } from '@mantine/core';
+import '@mantine/core/styles.css';
+import CarouselComponent from './carousel/carousel';
 
-  return (
-    <>
-    </>
-  )
+const myColor: MantineColorsTuple = [
+  '#fcf6e7',
+  '#f2ebda',
+  '#e1d6b9',
+  '#d0bf94',
+  '#c1ac74',
+  '#b89f60',
+  '#b49954',
+  '#9e8544',
+  '#8d7639',
+  '#7a652b'
+];
+
+const theme = createTheme({
+  colors: {
+    myColor,
+  }
+});
+
+export default function App() {
+  return <MantineProvider theme={theme}>
+    {
+      <div style={{ maxWidth: 800, margin: 'auto', padding: '20px' }}>
+        <CarouselComponent />
+      </div>
+    }
+    </MantineProvider>;
 }
-
-export default App
---
+++ src/carousel/carousel.tsx
@@ -0,0 +1,65 @@
+import { Carousel } from '@mantine/carousel';
+import { Card, Image } from '@mantine/core';
+import '@mantine/carousel/styles.css';
+
+const slides = [
+  {
+    image: '../../assets/page1.png',
+  },
+  {
+    image: '../../assets/page2.png',
+  },
+  {
+    image: '../../assets/page3.png',
+  },
+  {
+    image: '../../assets/page4.png',
+  },
+  {
+    image: '../../assets/page5.png',
+  },
+  {
+    image: '../../assets/page6.png',
+  },
+  {
+    image: '../../assets/page7.png',
+  },
+];
+
+function Slide({ image }) {
+  return (
+    <Card 
+      shadow="md" 
+      radius="md" 
+      padding="lg" 
+      withBorder
+    >
+      <Card.Section>
+        <Image src={image} width="100%" height="auto" />
+      </Card.Section>
+    </Card>
+  );
+}
+
+export default function CarouselComponent() {
+  return (
+    <Carousel
+      slideSize="100%"
+      breakpoints={[
+        { maxWidth: 'md', slideSize: '50%' },
+        { maxWidth: 'sm', slideSize: '100%' },
+      ]}
+      slideGap="md"
+      align="start"
+      loop
+      withIndicators
+      
+    >
+      {slides.map((slide, index) => (
+        <Carousel.Slide key={index}>
+          <Slide {...slide} />
+        </Carousel.Slide>
+      ))}
+    </Carousel>
+  );
+}
