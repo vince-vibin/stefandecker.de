f10b85ca0c2ae5fecf700d51f50217b129ed227a
vince-vibin <vincent.sternad@meconema.de>
2025-07-10 21:28:20 +0200

    add public assets

--- src/carousel/carousel.tsx
+++ src/carousel/carousel.tsx
@@ -1,15 +1,23 @@
 import { Carousel } from '@mantine/carousel';
 import { Card, Image } from '@mantine/core';
 import '@mantine/carousel/styles.css';
+import classes from "./carousel.module.css";
 
-const slides = [
-  'page1.png',
-  'page2.png',
-  'page3.png',
-  'page4.png',
-  'page5.png',
-  'page6.png',
-  'page7.png',
+const slides = [ // TODO make this dynamic
+  'page1.webp',
+  'page2.webp',
+  'page3.webp',
+  'page4.webp',
+  'page5.webp',
+  'page6.webp',
+  'page7.webp',
+  'page8.webp',
+  'page9.webp',
+  'page10.webp',
+  'page11.webp',
+  'page12.webp',
+  'page13.webp',
+  'page14.webp',
 ];
 
 type SlideProps = {
@@ -23,9 +31,10 @@ function Slide({ image }: SlideProps) {
       radius="md" 
       padding="lg" 
       withBorder
+      className={classes.card}
     >
       <Card.Section>
-        <Image src={`${import.meta.env.BASE_URL}assets/${image}`} width="100%" height="auto" />
+        <Image src={`${import.meta.env.BASE_URL}assets/portfolio/${image}`} width="100%" height="auto" />
       </Card.Section>
     </Card>
   );
@@ -34,12 +43,11 @@ function Slide({ image }: SlideProps) {
 export default function CarouselComponent() {
   return (
     <Carousel
-      mx="auto"
-      maw={1500}
       withControls
       slideSize="90%"
       slideGap="md"
       withIndicators
+      className={classes.carousel}
     >
       {slides.map((image) => (
         <Carousel.Slide key={image}>
